d58f2a54c41dd0309d650d4cfcec27c4129b2596
Douglas Souza <douglas_search@hotmail.com>
2025-07-29 19:40:34 -0300

    adjusting header logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header({ showAuth = true }: HeaderProps) {
                     alt="CassinoTrack Logo"
                     width={160}
                     height={160}
-                    className="h-20 w-auto"
+                    className="h-10 w-auto"
                 />
                 <span className="text-2xl font-bold tracking-widest hover:text-white transition hidden sm:block">
                     CassinoTrack
